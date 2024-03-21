a6a8461010132055098d2ef009231905b817a740
guifrancolelabs <guilhermefranco@lelabs.com.br>
2024-03-21 15:55:03 -0300

    (fix/site-1426): retirando span do texto de simulacao de frente

--- components/ui/ShippingSimulation.tsx
+++ components/ui/ShippingSimulation.tsx
@@ -63,11 +63,7 @@ function ShippingContent({ simulation }: {
           </span>
         </li>
       ))}
-      <span class="text-base-300">
-        Os prazos de entrega começam a contar a partir da confirmação do
-        pagamento e podem variar de acordo com a quantidade de produtos na
-        sacola.
-      </span>
+      
     </ul>
   );
 }
