ef2021e2bbc8f1caa059490ca3123c767031fccd
Gabriel Alexander <gabrielmendes@lelabs.com.br>
2024-03-28 09:06:32 -0300

    Update getDefaultVtexSeller.ts

--- utils/getDefaultVtexSeller.ts
+++ utils/getDefaultVtexSeller.ts
@@ -1,9 +1,7 @@
 import { Seller } from "deco-sites/casaevideo/types/vtexRestApi.ts";
 
 export function getDefaultVtexSeller(sellers?: Seller[]) {
-  if (!sellers || sellers.length === 0) {
-    return;
-  }
+  if (!sellers || sellers.length === 0) return;
 
   const defaultSeller = sellers.find((seller) => seller.sellerDefault);
 
