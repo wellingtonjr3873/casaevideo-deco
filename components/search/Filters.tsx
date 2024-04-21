4d67de65ed8bf930d1a2baadfbda60d0644e62e1
AbraaoMorais <moraisabraao@gmail.com>
2024-04-21 02:12:11 -0300

    refactor filters and add clean filters func

--- components/search/Filters.tsx
+++ components/search/Filters.tsx
@@ -1,14 +1,14 @@
 import Avatar from "$store/components/ui/Avatar.tsx";
 import { formatPrice } from "$store/sdk/format.ts";
+import { parseRange } from "apps/commerce/utils/filters.ts";
+import Icon from "deco-sites/casaevideo/components/ui/Icon.tsx";
+
 import type {
   Filter,
   FilterToggle,
   FilterToggleValue,
   ProductListingPage,
 } from "apps/commerce/types.ts";
-import { parseRange } from "apps/commerce/utils/filters.ts";
-import Icon from "deco-sites/casaevideo/components/ui/Icon.tsx";
-
 
 interface Props {
   filters: ProductListingPage["filters"];
@@ -17,119 +17,144 @@ interface Props {
 const isToggle = (filter: Filter): filter is FilterToggle =>
   filter["@type"] === "FilterToggle";
 
-function ValueItem(
-  { url, selected, label, quantity }: FilterToggleValue,
-) {
+function ValueItem({ url, selected, label, quantity }: FilterToggleValue) {
   return (
-    <li class="py-2">
-      <a href={url} rel="nofollow" class="flex items-center gap-2">
-        <div aria-checked={selected} class="checkbox checkbox-warning w-4 h-4 rounded-sm border-brand-secondary-200" />
-        <span class="text-sm text-neutral-900">{label}</span>
+    <li className="py-2 w-full">
+      <a href={url} rel="nofollow" className="flex items-center gap-2">
+        <div aria-checked={selected} className="checkbox checkbox-warning w-4 h-4 rounded-sm border-brand-secondary-200" />
+        <span className="text-sm text-neutral-900 w-full break-words max-w-40">{label}</span>
       </a>
     </li>
   );
 }
 
-
-function FilterValues({ key, values }: FilterToggle) {
-  const flexDirection = key === "tamanho" || key === "cor"
-    ? "flex-row"
-    : "flex-col";
-
+function FilterListSelected({ values }: FilterToggle) {
   return (
     <>
-      <ul class={`flex flex-wrap gap-2 ${flexDirection}`}>
-       
-        {values.map((item, index) => {
-          const { url, selected, value } = item;
-
-          if (index <= 4) {
-
-            if (key === "cor" || key === "tamanho") {
-              return (
-                <li>
-                  <a href={url} rel="nofollow">
-                    <Avatar
-                      content={value}
-                      variant={selected ? "active" : "default"}
-                    />
-                  </a>
-                </li>
-              );
-            }
-
-            if (key === "price") {
-              const range = parseRange(item.value);
-
-              return range && (
-                <ValueItem
-                  {...item}
-                  label={`${formatPrice(range.from)} - ${formatPrice(range.to)}`}
-                />
-              );
-            }
-
-            return <ValueItem {...item} />;
-          }
-        })}
-
-
-        {values.length > 4 && (
-          <details class="collapse">
-            <summary class="collapse-title text-xl font-medium">Click to open/close</summary>
-            {
-              values.map((item, index) => {
-
-                const { url, selected, value } = item;
-
-                if (key === "cor" || key === "tamanho") {
-                  return (
-                    <li>
-                      <a href={url} rel="nofollow">
-                        <Avatar
-                          content={value}
-                          variant={selected ? "active" : "default"}
-                        />
-                      </a>
-                    </li>
-                  );
-                }
-
-                if (index > 4) {
-                  return <ValueItem {...item} />;
-                }
-              })
-            }
-          </details>)
-          }
-      </ul>
+      {
+        values.map((item, index) => {
+          const { selected } = item;
+          return selected && (
+            <li key={`selected-item-${index}`} className="">
+              <a href={item.url} rel="nofollow" className="flex items-center justify-center gap-2 w-fit rounded-md border-2 border-brand-secondary-400 bg-brand-terciary-1 p-2">
+                <div aria-checked={selected} className="border rounded-full font-normal text-sm p-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M6 18L18 6M6 6l12 12" /></svg>
+                </div>
+                <span className="text-sm text-neutral-900 w-full break-words max-w-40">{item.label}</span>
+              </a>
+            </li>
+          );
+        })
+      }
 
     </>
+  )
+}
 
+function FilterValues({ key, values }: FilterToggle) {
+  const flexDirection = ["tamanho", "cor"].includes(key) ? "flex-row" : "flex-col";
+
+  return (
+    <ul className={`flex flex-wrap gap-2 ${flexDirection}`}>
+      {values.slice(0, 6).map((item, index) => {
+        const { url, selected, value } = item;
+
+        if (key === "cor" || key === "tamanho") {
+          return (
+            <li key={index}>
+              <a href={url} rel="nofollow">
+                <Avatar content={value} variant={selected ? "active" : "default"} />
+              </a>
+            </li>
+          );
+        }
+
+        if (key === "price") {
+          const range = parseRange(item.value);
+
+          return range && (
+            <ValueItem
+              key={index}
+              {...item}
+              label={`${formatPrice(range.from)} - ${formatPrice(range.to)}`}
+            />
+          );
+        }
+
+        return <ValueItem key={index} {...item} />;
+      })}
+
+      {values.length > 6 && (
+        <li className="w-full">
+          <details className={`collapse`}>
+            <summary className="w-full block pt-2 px-4 collapse-title text-right text-xl font-medium">Ver mais</summary>
+            <ul className="flex flex-wrap gap-2 flex-row">
+            {values.slice(6).map((item, index) => {
+              const { url, selected, value } = item;
+
+              if (key === "cor" || key === "tamanho") {
+                return (
+                  <li key={index}>
+                    <a href={url} rel="nofollow">
+                      <Avatar content={value} variant={selected ? "active" : "default"} />
+                    </a>
+                  </li>
+                );
+              }
+              return <ValueItem key={index} {...item} />;
+            })}
+            </ul>
+          </details>
+        </li>
+      )}
+    </ul>
   );
 }
 
+
 function Filters({ filters }: Props) {
+
+  //verifica se há pelo menos 1 filtro ativo
+  const hasFilterActive = filters.filter(isToggle).some(filter => filter.values.some(value => value.selected));
+
   return (
-    <ul class="flex flex-col gap-4">
-      {filters
-        .filter(isToggle)
-        .map((filter, index) => (
-          <li tabIndex={index} class="dropdown bg-neutral-50 py-3 px-5">
-            <details class="group">
-              <summary class="m-1 flex justify-between items-center cursor-pointer font-bold text-base">
-                {filter.label}
-                <span class="w-4 h-4 text-center flex justify-center items-center">
-                  <span class="group-open:-rotate-90 transition-all duration-200 ease-in">
-                    <Icon id="ArrowAccordion" size={24} strokeWidth={2} fill={`text-neutral-900`} />
+    <>
+      {
+        hasFilterActive && (
+          <div className="mb-2 bg-neutral-50 border border-brand-secondary-400">
+            <header className="px-4 py-2 text-left">
+              <p className="font-bold text-base">Fltros Selecionados</p>
+            </header>
+            <ul className="flex flex-wrap justify-start items-center gap-2 px-4 py-2">
+              {
+                filters.filter(isToggle).map((filter) => {
+                  return <FilterListSelected  {...filter} />
+                })
+              }
+            </ul>
+            <a href="?" className="block w-full text-right py-2 px-4 text-base font-bold">Limpar Filtros</a>
+          </div>
+        )
+      }
+      <ul className="flex flex-col gap-4">
+        {
+          filters.filter(isToggle).map((filter, index) => (
+            <li key={index} className="dropdown bg-neutral-50 py-3 px-5">
+              <details className="group">
+                <summary className="m-1 flex justify-between items-center cursor-pointer font-bold text-base pb-3">
+                  {filter.label}
+                  <span className="w-4 h-4 text-center flex justify-center items-center">
+                    <span className="group-open:-rotate-90 transition-all duration-200 ease-in">
+                      <Icon id="ArrowAccordion" size={24} strokeWidth={2} fill="text-neutral-900" />
+                    </span>
                   </span>
-                </span>
-              </summary>
-              <FilterValues {...filter} />
-            </details>
-          </li>
-        ))}
-    </ul>
+                </summary>
+                <FilterValues {...filter} />
+              </details>
+            </li>
+          ))}
+      </ul>
+    </>
   );
 }
 
--- components/ui/Avatar.tsx
+++ components/ui/Avatar.tsx
@@ -4,16 +4,37 @@
  */
 
 const colors: Record<string, string> = {
-  "azul-clara": "bg-[#87CEFA] ring-[#87CEFA]",
-  "azul-marinho": "bg-[#000080] ring-[#000080]",
-  "branca": "bg-[#FFFFFF] ring-[#FFFFFF]",
-  "cinza": "bg-[#808080] ring-[#808080]",
-  "cinza-escura": "bg-[#A9A9A9] ring-[#A9A9A9]",
-  "laranja": "bg-[#FFA500] ring-[#FFA500]",
-  "marrom": "bg-[#A52A2A] ring-[#A52A2A]",
-  "preta": "bg-[#161616] ring-[#161616]",
-  "verde-clara": "bg-[#90EE90] ring-[#90EE90]",
-  "vermelha": "bg-[#FF0000] ring-[#FF0000]",
+  "amarela"         : "bg-brand-terciary-1 ring-brand-terciary-1",
+  "azul"            : "bg-[#4169E1] ring-[#4169E1]",
+  "azul-claro"      : "bg-[#87CEFA] ring-[#87CEFA]",
+  "azul-escuro"     : "bg-[#00008B] ring-[#00008B]",
+  "azul-marinho"    : "bg-[#000080] ring-[#000080]",
+  "azul-bebe"       : "bg-[#87CEEB] ring-[#87CEEB]",
+  "branca"          : "bg-[#FFFFFF] ring-[#FFFFFF]","branco": "bg-[#FFFFFF] ring-[#FFFFFF]",
+  "cinza"           : "bg-[#808080] ring-[#808080]",
+  "cinza-escura"    : "bg-[#A9A9A9] ring-[#A9A9A9]",
+  "dourado"         : "bg-[#FFAC33] ring-[#FFAC33]",
+  "dourada"         : "bg-[#FFAC33] ring-[#FFAC33]",
+  "grafite"         : "bg-[#2F4F4F] ring-[#2F4F4F]",
+  "laranja"         : "bg-[#FF5E00] ring-[#FF5E00]",
+  "lilac"           : "bg-[#C8A2C8] ring-[#C8A2C8]",
+  "lilas"           : "bg-[#C8A2C8] ring-[#C8A2C8]",
+  "marrom"          : "bg-[#8B4513] ring-[#8B4513]",
+  "marsala"         : "bg-[#8B4513] ring-[#8B4513]",
+  "nude"            : "bg-[#D2B48C] ring-[#D2B48C]",
+  "preta"           : "bg-[#161616] ring-[#161616]","preto":"bg-[#161616] ring-[#161616]",
+  "preta-prata"     : "bg-[#464444] ring-[#464444]","preto-prata": "bg-[#464444] ring-[#464444]",
+  "prata"           : "bg-[#C0C0C0] ring-[#C0C0C0]",
+  "rose"            : "bg-[#FFC0CB] ring-[#FF69B4]","rosa": "bg-[#FFC0CB] ring-[#FF69B4]",
+  "roxa"            : "bg-[#800080] ring-[#800080]",
+  "roxo"            : "bg-[#6A0DAD] ring-[#6A0DAD]",
+  "tita-nio"        : "bg-[#778899] ring-[#778899]",
+  "verde"           : "bg-[#008000] ring-[#008000]",
+  "verde-claro"     : "bg-[#90EE90] ring-[#90EE90]",
+  "vermelho"        : "bg-[#FF0000] ring-[#FF0000]","vermelha": "bg-[#FF0000] ring-[#FF0000]",
+  "vermelho-magenta": "bg-[#FF00FF] ring-[#FF00FF]",
+  "violeta"         : "bg-[#8A2BE2] ring-[#701DBD]",
+  "warm-gray-coza"  : "bg-[#C4BEBE] ring-[#C4BEBE]",
 
   // Color variants - only applied when no color as content is passed
   "active": "bg-neutral-50 text-brand-primary-700",
@@ -27,10 +48,10 @@ interface Props {
 }
 
 const variants = {
-  active: "text-brand-primary-700 border-brand-primary-700 border-2",
+  active: "text-brand-terciary-1 border-brand-terciary-1 border-2",
   disabled:
     `relative after:absolute after:left-0 after:top-1/2 after:h-[1px] after:bg-red-800 after:w-full after:block after:-rotate-45 after:content-[""]`,
-  default: "border border-brand-secondary-500 hover:border-brand-primary-700",
+  default: "border border-brand-secondary-100 hover:border-brand-terciary-1",
 };
 
 function Avatar({ content, variant = "default" }: Props) {
@@ -49,7 +70,7 @@ function Avatar({ content, variant = "default" }: Props) {
         {isColor ? (
           <img src={imageUrl} alt={imageContent.replace("Cor: ", "")} />
         ) : (
-          <span class={`uppercase`}>
+          <span class={`uppercase text-xs`}>
             {colors[content] ? "" : content}
           </span>
         )} 
