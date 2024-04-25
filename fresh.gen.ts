299a148cc6f2b966b78f27208eecccda1051c056
wellington3873 <wellingtonrufino@lelabs.com.br>
2024-04-25 18:11:19 -0300

    fix(site-1543): user feedback in header

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -25,12 +25,10 @@ import * as $Header_Cart_wake from "./islands/Header/Cart/wake.tsx";
 import * as $Header_Drawers from "./islands/Header/Drawers.tsx";
 import * as $Header_GeoLocationPointBar_GeoLocationPoint from "./islands/Header/GeoLocationPointBar/GeoLocationPoint.tsx";
 import * as $Header_GeoLocationPointBar_GeoLocationPointBar from "./islands/Header/GeoLocationPointBar/GeoLocationPointBar.tsx";
+import * as $Header_LoggedUser from "./islands/Header/LoggedUser.tsx";
 import * as $Header_Searchbar from "./islands/Header/Searchbar.tsx";
-
 import * as $ImageZoom from "./islands/ImageZoom.tsx";
-
 import * as $Installments from "./islands/Installments.tsx";
-
 import * as $Newsletter from "./islands/Newsletter.tsx";
 import * as $OursStores from "./islands/OursStores.tsx";
 import * as $OutOfStock from "./islands/OutOfStock.tsx";
@@ -78,12 +76,10 @@ const manifest = {
       $Header_GeoLocationPointBar_GeoLocationPoint,
     "./islands/Header/GeoLocationPointBar/GeoLocationPointBar.tsx":
       $Header_GeoLocationPointBar_GeoLocationPointBar,
+    "./islands/Header/LoggedUser.tsx": $Header_LoggedUser,
     "./islands/Header/Searchbar.tsx": $Header_Searchbar,
-
     "./islands/ImageZoom.tsx": $ImageZoom,
-
     "./islands/Installments.tsx": $Installments,
-
     "./islands/Newsletter.tsx": $Newsletter,
     "./islands/OursStores.tsx": $OursStores,
     "./islands/OutOfStock.tsx": $OutOfStock,
