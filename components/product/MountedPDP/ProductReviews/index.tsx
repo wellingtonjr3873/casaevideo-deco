47dd4bef81bd8464181a8aef357a48a570f59c50
guifrancolelabs <guilhermefranco@lelabs.com.br>
2024-05-15 08:37:57 -0300

    retirando console.log

--- components/product/MountedPDP/ProductReviews/index.tsx
+++ components/product/MountedPDP/ProductReviews/index.tsx
@@ -24,8 +24,6 @@ function ProductReviews(props: Props) {
   const { page } = props;
   const correctPage = page as PageProps;
 
-  console.log(page, "page >>>>>>>>>>>>>>>>>")
-
   if (correctPage === null) {
     throw new Error("Missing Product Details Page Info");
   }
