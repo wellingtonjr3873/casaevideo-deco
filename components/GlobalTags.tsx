de0fd8bd1c3a5dd24e5733b39f5a5020a59a09ef
LeHenriqueAlmeida <henriquexavier@lelabs.com.br>
2024-05-27 07:43:39 -0300

    [fix/site-1688] Removendo link rel do componente GlobalTags para apenas trocar o favicon dentro da pasta static

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -7,9 +7,6 @@ export interface Props {
 function GlobalTags({ url }: Props) {
   return (
     <Head>
-      {/* Store favicon */}
-      <link rel="icon" href={"https://casaevideonewio.vteximg.com.br/arquivos/fav-icon-cev.png"} />
-
       {/* Enable View Transitions API */}
       <meta name="view-transition" content="same-origin" />
 
