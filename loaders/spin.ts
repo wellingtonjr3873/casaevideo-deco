032bea4ca6b86043120573d072196a18de8074da
wellington3873 <wellingtonrufino@lelabs.com.br>
2024-07-03 12:35:31 -0300

    update providers

--- loaders/spin.ts
+++ loaders/spin.ts
@@ -1,20 +1,17 @@
 
 import { logger } from "deco/observability/otel/config.ts";
 import { ROLETA_API_URL } from "deco-sites/casaevideo/constants.tsx";
+import { Res } from "deco-sites/casaevideo/types/api-roleta.d.ts";
 type Props = {
   email: string;
 };
-type Res = {
-  error: boolean;
-  value?: {
-    clusterWinned: string
-  }
-  message?: string;
-}
+
 async function action(
   props: Props,
   _req: Request,
-): Promise<Res> {
+): Promise<Res<{
+  clusterWinned: string
+}>> {
   const url = `${ROLETA_API_URL}/roleta-black-friday/spin`;
   const requestOptions = {
     method: "POST",
@@ -22,14 +19,14 @@ async function action(
       Accept: "application/json",
       "Cache-Control": "no-cache",
       "Content-Type": "application/json",
-      "x-provider": "lebiscuit", //MUDE O VENDOR
+      "x-provider": "casaevideonewio", //MUDE O VENDOR
     },
     body: JSON.stringify({ email: props.email }),
   };
 
   try {
     const response = await fetch(url, requestOptions);
-    const data: Res = await response.json();
+    const data = await response.json();
     return data;
   } catch (error) {
     logger.error(error.message)
