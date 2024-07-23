6576ecf7bef450f61ca3baa8be069447205f189b
wellington3873 <wellingtonrufino@lelabs.com.br>
2024-07-23 17:41:32 -0300

    update apple-app-site-association

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,15 +1,14 @@
 import { FreshContext } from "$fresh/server.ts";
 const APPLE_DEEP_LINK = "apple-app-site-association";
 export async function handler(req: Request, ctx: FreshContext<any>){
-    const referUrl = req.headers.get('referer');
-    const file  = await Deno.readFile('apple-app-site-association');
+    const referUrl = req.url;
     if(referUrl && referUrl.includes(APPLE_DEEP_LINK)){
-        const resp = await ctx.next();
-        resp.headers.set("Content-Type", "application/json");
+        
+        const file  = await Deno.readFile('apple-app-site-association');
+        const response = new Response(file)
+        response.headers.set("Content-Type", "application/json");
         return new Response(file)
     }
 
-    const resp = await ctx.next();
-
-return resp
+return await ctx.next();
 }
